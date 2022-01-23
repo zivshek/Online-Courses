ef223c6fc40f55a2b2a79d6abd5dac85bb997af1
zivshek <ziv.hao.shek@gmail.com>
2022-01-23 12:45:16 -0500

    PL-B 5-10, done

--- Programming Languages B/hw4.rkt
+++ Programming Languages B/hw4.rkt
@@ -21,9 +21,65 @@
 
 ; 4
 (define (stream-for-n-steps s n)
-  (letrec ([f (lambda (s ans)
-                (let ([pr (s)])
-                  (if (= (length ans) n)
-                      ans
-                      (f (cdr pr) (cons (car pr) ans)))))])
-    (f s '())))
+  (if (= n 0)
+      null
+      (let ([pr (s)])
+        (cons (car pr) (stream-for-n-steps (cdr pr) (- n 1))))))
+
+; 5
+(define funny-number-stream
+  (letrec ([f (lambda (x)
+                (cons (if (= (remainder x 5) 0) (- x) x)
+                      (lambda () (f (+ x 1)))))])
+    (lambda () (f 1))))
+
+; 6
+(define dan-then-dog
+  (letrec ([f (lambda (n)
+                (cons (if (= (remainder n 2) 0) "dan.jpg" "dog.jpg")
+                      (lambda () (f (+ n 1)))))])
+    (lambda () (f 0))))
+
+; 7
+(define (stream-add-zero s)
+  (lambda ()
+    (let ([pr (s)])
+      (cons (cons 0 (car pr)) (stream-add-zero (cdr pr))))))
+
+; 8
+(define (cycle-lists xs ys)
+  (letrec ([f (lambda (n)
+                (cons (cons (list-nth-mod xs n) (list-nth-mod ys n))
+                      (lambda () (f (+ n 1)))))])
+    (lambda () (f 0))))
+
+; 9
+(define (vector-assoc v vec)
+  (letrec ([f (lambda (vec)
+                (cond [(or (null? vec) (vector-empty? vec)) #f]
+                      [#t (letrec ([len (vector-length vec)]
+                                   [hd (vector-ref vec 0)]
+                                   [tail (vector-take-right vec (- len 1))])
+                          (if (and (pair? hd) (= (car hd) v)) hd (f tail)))]))])
+    (f vec)))
+                      
+; 10
+(define (cached-assoc xs n)
+  (letrec ([cache (make-vector n #f)]
+           [count 0]
+           [f (lambda (v)
+                (let ([result (vector-assoc v cache)])
+                  (if (equal? result #t)
+                      result
+                      (let ([new-result (assoc v xs)])
+                        (begin
+                          (if (equal? new-result #t)
+                              (begin
+                                (vector-set! cache (remainder count n) new-result)
+                                (set! count (+ count 1))
+                                new-result)
+                              new-result))))))])
+    f))
+    
+                      
+                  
